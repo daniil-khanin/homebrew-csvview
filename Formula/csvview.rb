37127b0c21d6d5b761359ac23e7b7030a0ea6ee0
daniil.khanin <daniil.khanin@larixon.com>
2026-03-18 23:02:13 +0100

    csvview 13

--- Formula/csvview.rb
+++ Formula/csvview.rb
@@ -1,10 +1,10 @@
 class Csvview < Formula
   desc "Fast interactive CSV viewer and editor for the terminal"
   homepage "https://github.com/daniil-khanin/csvview"
-  url "https://github.com/daniil-khanin/csvview/archive/refs/tags/v12.tar.gz"
-  sha256 "9290e7c04c2c2fb8dda1a47f7a18e2c6c789ecc3d4ed59d9a02e34ad9ccbcb1b"
+  url "https://github.com/daniil-khanin/csvview/archive/refs/tags/v13.tar.gz"
+  sha256 "87ae1f3a7c03935834a2a49e61f7fa99f267d944cd38f0b39ee0b3e86b6d7d17"
   license "MIT"
-  version "12"
+  version "13"
 
   depends_on "ncurses"
 
