a39b340f7eba7b4b054d107345e979dbb05eca85
daniil.khanin <daniil.khanin@larixon.com>
2026-03-18 18:15:46 +0100

    Fix sha256 checksum after repo made public

--- Formula/csvview.rb
+++ Formula/csvview.rb
@@ -2,7 +2,7 @@ class Csvview < Formula
   desc "Fast interactive CSV viewer and editor for the terminal"
   homepage "https://github.com/daniil-khanin/csvview"
   url "https://github.com/daniil-khanin/csvview/archive/refs/tags/v12.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "9290e7c04c2c2fb8dda1a47f7a18e2c6c789ecc3d4ed59d9a02e34ad9ccbcb1b"
   license "MIT"
   version "12"
 
